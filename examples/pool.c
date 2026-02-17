71d913788dfba6ba346f945e8a3068424826fdde
amuerta <amurtasmail@gmail.com>
2026-02-17 17:26:49 +0200

    Updated Pool data structure API, again.

--- examples/pool.c
+++ examples/pool.c
@@ -1,36 +1,52 @@
-#define POOL_USE_EXACT_TYPE_MATCH
+#define HC_POOL_STRIP_PREFIX
 #include "../src/pool.h"
 
 // #define Pool hc_Pool
 
-typedef struct EntityData {
-    int i;
-} EntityData;
-
 typedef struct {
-    EntityData data;
+    struct {int i;} data;
 } Entity;
 
 typedef hc_Pool(Entity) EntPool;
 
 void pool_grow_if_needed(EntPool* pool) {
     if(pool_need_resize(pool)) {
-        size_t newsz = pool_measure(pool->capacity * 2, hc_pool_type_size(pool));
+        size_t newsz = pool_measure(*pool, pool->capacity * 2);
         free(pool_grow_buffer(pool, calloc(newsz,1), newsz));
         pool->allocated_on_heap = true;
     }
 }
 
+#define Pool hc_Pool
+
+// int main(void) {
+//     static char buffer[1<<10];
+//     Pool(int) pool = {0};
+//     pool_from_buffer(&pool, buffer, sizeof(buffer));
+//
+//     for(int i = 0; i < 10; i++) {
+//         pool_insert(&pool, &i);
+//     }
+//
+//     printf("cap:    %lu\n", pool.capacity);
+//     printf("max_c:  %lu\n", pool.max_count);
+//
+//     for(size_t i = 0; i < pool.max_count; i++) {
+//         // if(hc_pool_is_slot_used(pool.items[i])) 
+//             printf("e%02lu : %i\n", i, hc_pool_get(pool, i));
+//     }
+// }
+
 // TODO: fully make this example work
+
+
+
+
 int main(void) {
-    printf("Running pool example\n");
-    static char buffer[1<<10];
     EntPool pool = {0};
+    hc_pool_index ids[20] = {0};
+   
     pool_from_heap(&pool, 2);
-    
-    pool_index ids[20] = {0};
-    (void)ids;
-    (void)buffer;
 
     for(int i = 0; i < 20; i++) {
         pool_grow_if_needed(&pool);
@@ -38,21 +54,25 @@ int main(void) {
         ids[i] = pool_insert(&pool, &e);
     }
 
-    // Entity *e;
-    // for(int i = 0; pool_next(&pool, &e); i++) {
-    //     if(e) printf("e%02i : %i\n", i, e->data.i);
-    //     e = 0;
-    // }
+    pool_release(&pool, ids[3]);
+    pool_release(&pool, ids[4]);
+    pool_release(&pool, ids[5]);
 
-    size_t m = pool_measure(2, sizeof(Entity));
+    for(int i = 20; i < 20+3; i++) {
+        pool_insert(&pool, &i);
+    }
+
+    // __asm__("int3");
+    hc_pool_foreach(pool, i) {
+        printf("e%02lu : %i\n", i, hc_pool_get(pool, i).data.i);
+    }
+
+    size_t m = pool_measure(pool, 2);
     free(pool_resize_buffer(&pool, calloc(m,1), m));
 
-    // printf("After resize: \n");
-    //
-    // for(int i = 0; pool_next(&pool, &e); i++) {
-    //     if(e) printf("e%02i : %i\n", i, e->data.i);
-    //     e = 0;
-    // }
+    hc_pool_foreach(pool, i) {
+        printf("e%02lu : %i\n", i, hc_pool_get(pool, i).data.i);
+    }
 
     free(pool.items);
 }
--- src/pool.h
+++ src/pool.h
@@ -1,15 +1,16 @@
-#ifndef __EOBJECT_POOL_H
-#define __EOBJECT_POOL_H
+#ifndef __HC_POOL_H
+#define __HC_POOL_H
 
 /*  POOL DATA STRUCTURE
  *
- *  Pool keeps location of objects in memory consistent and you access them via pool_index instead of pointer, even tho using a pointer
- *  is a valid method too. Benefit of Pool is that when working with large number of Entities, insertion and deletion operations take `O(1)`.
+ *  Pool keeps location of objects in memory consistent and you access them via hc_pool_get instead of trying to use pointers,
+ *  Benefit of Pool is that when working with large number of Entities, insertion and deletion operations take `O(1)`.
  *  
  *  NOTES:
  *      + resizing `Pool` doesn't update `free_list` as this operation can be confusing or ambiguous depending on your way of use. So when you downsize
  *          your pool - migrating is done manually.
- *      + API of pool is designed to follow my "allocator free" design idea, thus you work with buffers that are fed into pool, instead of giving/defining allocator functions. 
+ *      + API of pool is designed to follow my "allocator free" design idea, you work with buffers that are fed into data strucutre, 
+ *          instead of giving function pointers to allocator functions. 
  *          Link to my post about this idea: <TBD>
  * */
 
@@ -20,24 +21,25 @@
 #include <assert.h>
 
 #define hc_Pool(T) struct {                                 \
-    struct { char infomask; T item;} *items;                \
-    pool_index      *free_list;                             \
+    struct test     { int infomask; T item;} *items;        \
+    hc_pool_index   *free_list;                             \
     size_t          count, capacity, max_count, free_count; \
     bool            allocated_on_heap;                      \
 }
 
 #define HC_POOL_INVALID_INDEX ((size_t)(-1))
-typedef size_t pool_index;
+typedef size_t hc_pool_index;
+typedef int    hc_pool_bitmask8;
 
 // PoolSlot is a generic container to fit your data
 typedef struct {
-    unsigned char   infomask; // bool
-    unsigned char   data[];
+    int             infomask; // bool
+    char            data[];   
 } hc_PoolSlotBase;
 
 typedef struct {
-    hc_PoolSlotBase *items;
-    pool_index      *free_list;
+    void            *items;
+    hc_pool_index   *free_list;
     size_t          count, capacity, 
                     max_count, free_count;
     bool            allocated_on_heap;
@@ -49,42 +51,93 @@ enum {
 };
 
 
-#define hc_pool_type_size(P) sizeof(((P)->items[0].item))
+#define hc_pool_slot_size(P)        sizeof((P).items[0])
+#define hc_pool_type_size(P)        sizeof((P).items[0].item)
+#define hc_pool_end(P)              (P.max_count)
+#define hc_pool_is_slot_used(S)     (S.infomask & HC_POOL_SLOT_IS_USED)
+#define hc_pool_get(P, i)           ((P).items[hc_pool_assert_index(i)].item)
+#define hc_pool_measure(T, n)       hc_pool_measure_ex(sizeof((T).items[0]), n)
 
-#define pool_get(P, i)\
-    ((P)->items[pool_assert_index(i)].item)
+#define hc_pool_reserve(P)\
+    hc_pool_reserve_ex(P,\
+            hc_pool_slot_size(*(P)),\
+            hc_pool_type_size(*(P)))
 
-#define pool_insert(P, I)\
-    pool_insert_ex(P, hc_pool_type_size(P), I, sizeof(*(I)))
+#define hc_pool_release(P, I)\
+    hc_pool_release_ex(P,\
+            hc_pool_slot_size(*(P)),\
+            hc_pool_type_size(*(P)),\
+            I)
 
-#define pool_from_heap(P, count) \
-    pool_from_heap_ex(P, hc_pool_type_size(P), count)
+#define hc_pool_insert(P, I)\
+    hc_pool_insert_ex(P,\
+            hc_pool_slot_size(*(P)),\
+            hc_pool_type_size(*(P)),\
+            I, sizeof(*(I)))
 
-#define pool_from_buffer(P, buf, size) \
-    pool_from_buffer_ex(P, hc_pool_type_size(P), buf, size)
+#define hc_pool_from_heap(P, count) \
+    hc_pool_from_heap_ex(P,\
+            hc_pool_slot_size(*(P)),\
+            hc_pool_type_size(*(P)),\
+            count)
 
-#define pool_resize_buffer(P, new_buf, size)\
-    pool_resize_buffer_ex(P, hc_pool_type_size(P), new_buf, size)
+#define hc_pool_from_buffer(P, buf, size) \
+    hc_pool_from_buffer_ex(P,\
+            hc_pool_slot_size(*(P)),\
+            hc_pool_type_size(*(P)),\
+            buf, size)
 
-#define pool_grow_buffer(P, new_buf, size)\
-    pool_grow_buffer_ex(P, hc_pool_type_size(P), new_buf, size)
+#define hc_pool_resize_buffer(P, new_buf, size)\
+    hc_pool_resize_buffer_ex(P,\
+            hc_pool_slot_size(*(P)),\
+            hc_pool_type_size(*(P)),\
+            new_buf, size)
+
+#define hc_pool_grow_buffer(P, new_buf, size)\
+    hc_pool_grow_buffer_ex(P, \
+            hc_pool_slot_size(*(P)),\
+            hc_pool_type_size(*(P)),\
+            new_buf, size)
+
+#define hc_pool_foreach(P, I) \
+    for(size_t I = 0; i < hc_pool_end(P); I++)\
+    if(hc_pool_is_slot_used((P).items[I]))
+
+#define hc_pool_max(A,B) (((A) > (B)) ? (A) : (B))
+#define hc_pool_min(A,B) (((A) < (B)) ? (A) : (B))
 
 
-#define pool_max(A,B) (((A) > (B)) ? (A) : (B))
-#define pool_min(A,B) (((A) < (B)) ? (A) : (B))
+size_t          hc_pool_measure_ex(size_t slotsize, size_t count);
+bool            hc_pool_is_index_ok(hc_pool_index i);
+size_t          hc_pool_assert_index(hc_pool_index i);
+bool            hc_pool_has_space(void* pool);
+bool            hc_pool_need_resize(void* pool);
 
+void            hc_pool_from_buffer_ex(void* p, size_t slotsize, size_t typesize, void* buffer, size_t buffer_size) ;
+void            hc_pool_from_heap_ex(void* p, size_t slotsize, size_t typesize, size_t count) ;
+
+void*           hc_pool_resize_buffer_ex(void* p, size_t slotsize, size_t typesize, void* new_buffer, size_t new_buffer_size);
+void*           hc_pool_grow_buffer_ex(void* p, size_t slotsize, size_t typesize, void* new_buffer, size_t new_buffer_size);
+inline void     hc_pool_heap_free(void* p);
+
+hc_pool_index   hc_pool_reserve_ex(void* pool, size_t slotsize, size_t typesize);
+hc_pool_index   hc_pool_insert_ex(void* pool, size_t slotsize, size_t typesize, void* item, size_t size);
+void            hc_pool_release_ex(void* pool, size_t slotsize, size_t typesize, hc_pool_index i);
 
 #ifndef POOL_HEADER_ONLY
 
 // creates Pool that fits into given buffer, lazy way to create a pool out of
 // static buffer, for testing purpouses.
 
-void pool_from_buffer_ex(void* p, size_t typesize, void* buffer, size_t buffer_size) {
+void hc_pool_from_buffer_ex(void* p, size_t slotsize, size_t typesize, void* buffer, size_t buffer_size) {
     assert(p && "expected valid pointer too pool");
     hc_PoolBase *pool = p;
+
+    // TODO:
+    (void) typesize;
     
     size_t indexsize        = sizeof(pool->free_list[0]);
-    size_t full_typesize    = sizeof(pool->items[0]) + typesize;
+    size_t full_typesize    = slotsize;
     // float  split_ratio      = pool_max(full_typesize, indexsize) / pool_min(full_typesize, indexsize);
     size_t count_items_fit  = buffer_size / (full_typesize + indexsize);
     // split buffer between free_list and items:
@@ -102,7 +155,7 @@ void pool_from_buffer_ex(void* p, size_t typesize, void* buffer, size_t buffer_s
 }
 
 
-void pool_from_heap_ex(void* p, size_t typesize, size_t count) {
+void hc_pool_from_heap_ex(void* p, size_t slotsize, size_t typesize, size_t count) {
     assert(p && "expected valid pointer too pool");
     assert(count && typesize);
     hc_PoolBase *pool = p;
@@ -112,20 +165,19 @@ void pool_from_heap_ex(void* p, size_t typesize, size_t count) {
     assert(item_size && free_list_size);
 
     void* data = calloc(count, item_size + free_list_size); 
-    pool_from_buffer_ex(p, typesize, data, item_size+free_list_size);
+    hc_pool_from_buffer_ex(p, slotsize, typesize, data, item_size+free_list_size);
     pool->allocated_on_heap = true;
 }
 
-size_t pool_measure(size_t count, size_t typesize);
 
-size_t pool_measure(size_t count, size_t typesize) {
+
+size_t hc_pool_measure_ex(size_t slotsize, size_t count) {
     const hc_PoolBase       pool;
-    const hc_PoolSlotBase   slot;
-    return count*(typesize + sizeof(slot)) 
+    return count*slotsize 
          + count*sizeof(pool.free_list[0]);
 }
 
-void* pool_resize_buffer_ex(void* p, size_t typesize, void* new_buffer, size_t new_buffer_size) {
+void* hc_pool_resize_buffer_ex(void* p, size_t slotsize, size_t typesize, void* new_buffer, size_t new_buffer_size) {
     hc_PoolBase *pool   = p;
     hc_PoolBase copy    = *pool;
     void* old_ptr       = pool->items;
@@ -138,14 +190,14 @@ void* pool_resize_buffer_ex(void* p, size_t typesize, void* new_buffer, size_t n
            new_items_size = 0,
            new_free_list_size = 0;
 
-    old_items_size       = (typesize + sizeof(pool->items[0]))*pool->capacity;
-    old_free_list_size   = sizeof(pool->free_list[0])*pool->capacity;
-    pool_from_buffer_ex(pool, typesize, new_buffer, new_buffer_size);
-    new_items_size       = (typesize + sizeof(pool->items[0]))*pool->capacity;
-    new_free_list_size   = sizeof(pool->free_list[0])*pool->capacity;
+    old_items_size       = slotsize * pool->capacity;
+    old_free_list_size   = sizeof(pool->free_list[0]) * pool->capacity;
+    hc_pool_from_buffer_ex(pool, slotsize, typesize, new_buffer, new_buffer_size);
+    new_items_size       = slotsize * pool->capacity;
+    new_free_list_size   = sizeof(pool->free_list[0]) * pool->capacity;
 
-    size_t items_size     = pool_min(old_items_size, new_items_size);
-    size_t free_list_size = pool_min(old_free_list_size, new_free_list_size);
+    size_t items_size     = hc_pool_min(old_items_size, new_items_size);
+    size_t free_list_size = hc_pool_min(old_free_list_size, new_free_list_size);
 
     if(old_items_size > new_items_size) {
         pool->max_count = pool->capacity;
@@ -159,59 +211,43 @@ void* pool_resize_buffer_ex(void* p, size_t typesize, void* new_buffer, size_t n
     return old_ptr;
 }
 
-void* pool_grow_buffer_ex(void* p, size_t typesize, void* new_buffer, size_t new_buffer_size) {
+void* hc_pool_grow_buffer_ex(void* p, size_t slotsize, size_t typesize, void* new_buffer, size_t new_buffer_size) {
     hc_PoolBase *pool = p;
-    assert(pool_measure(pool->capacity, typesize) < new_buffer_size && "`pool_grow_buffer` was given smaller buffer size then before.");
-    return pool_resize_buffer_ex(p, typesize, new_buffer, new_buffer_size);
+    assert(hc_pool_measure_ex(slotsize, pool->capacity ) < new_buffer_size && "`pool_grow_buffer` was given smaller buffer size then before.");
+    return hc_pool_resize_buffer_ex(p, slotsize, typesize, new_buffer, new_buffer_size);
 }
 
 
-inline void pool_heap_free(void* p) {
+inline void hc_pool_heap_free(void* p) {
     hc_PoolBase *pool = p;
     assert(pool->allocated_on_heap);
     if(pool->items) free(pool->items);
 }
 
-// TODO: slightly improve performace by stopping after seeing the last item count wise.
-bool pool_ref_next(void* pool, size_t typesize, void** out) {
-    (void) typesize;
-    hc_PoolBase *p = pool;
-    static pool_index id = 0;
-    if(id >= p->capacity) {
-        id = 0;
-        return false;
-    }
-    void* ptr = p->items + id * sizeof(p->items[0]);
-    if(ptr) *out = ptr;
-    id++;
-    return true;
-}
-
-bool pool_check_index(pool_index i) {
+bool hc_pool_is_index_ok(hc_pool_index i) {
     return i != HC_POOL_INVALID_INDEX;
 }
 
-void pool_assert_index(pool_index i) {
-    assert(pool_check_index(i) && "Attempt to access unset item");
+size_t hc_pool_assert_index(hc_pool_index i) {
+    assert(hc_pool_is_index_ok(i) && "Attempt to access unset item");
+    return i;
 }
 
     
-bool pool_has_space(void* pool) {
+bool hc_pool_has_space(void* pool) {
     hc_PoolBase *p = pool;
     return p->count < p->capacity;
 }
 
-bool pool_need_resize(void* pool) {
+bool hc_pool_need_resize(void* pool) {
     hc_PoolBase *p = pool;
     return p->count >= p->capacity;
 }
 
-
-
-pool_index pool_reserve(void* pool, size_t typesize) {
+hc_pool_index hc_pool_reserve_ex(void* pool, size_t slotsize, size_t typesize) {
     hc_PoolBase *p = pool;
     assert(p->items && p->free_list && p->capacity && typesize);
-    pool_index index = (size_t)(-1);
+    hc_pool_index index = (size_t)(-1);
     if (p->free_count > 0) {
         index = p->free_list[p->free_count-1];
         p->free_count--;
@@ -221,8 +257,7 @@ pool_index pool_reserve(void* pool, size_t typesize) {
     assert(index != HC_POOL_INVALID_INDEX   && "Failed to reserve entity");
     assert(p->count < p->capacity           && "Attempt to buffer overflow");
 
-    size_t fullsize = sizeof(p->items[0]) + typesize ;
-    hc_PoolSlotBase* item = p->items + fullsize*index;
+    hc_PoolSlotBase* item = p->items + slotsize*index;
     item->infomask = HC_POOL_SLOT_IS_USED;
     p->count++;
     
@@ -232,15 +267,14 @@ pool_index pool_reserve(void* pool, size_t typesize) {
     return index;
 }
 
-void pool_release(void* pool, size_t typesize, pool_index i) {
+void hc_pool_release_ex(void* pool, size_t slotsize, size_t typesize, hc_pool_index i) {
     hc_PoolBase *p = pool;
     assert(p->items && p->free_list && p->capacity && typesize);
     if (p->count==0)
         return;
     assert(i < p->capacity && "Attempt to access Out of Bounds");
 
-    size_t fullsize = sizeof(p->items[0]) + typesize ;
-    hc_PoolSlotBase* item = (p->items + i*fullsize);
+    hc_PoolSlotBase* item = (p->items + i*slotsize);
 
     if (!item->infomask) return;
 
@@ -252,16 +286,55 @@ void pool_release(void* pool, size_t typesize, pool_index i) {
 }
 
 
-pool_index pool_insert_ex(void* pool, size_t typesize, void* item, size_t size) {
+hc_pool_index hc_pool_insert_ex(void* pool, size_t slotsize, size_t typesize, void* item, size_t size) {
     hc_PoolBase *p = pool;
     assert(typesize == size);
-    pool_index i = pool_reserve(pool, typesize);
-    size_t fullsize = sizeof(p->items[0]) + typesize ;
-    void* it = p->items + fullsize*i;
-    memcpy(it, item, size);
+    hc_pool_index i = hc_pool_reserve_ex(pool, slotsize, typesize);
+    if(hc_pool_is_index_ok(i)) {
+        void* slot      = p->items + slotsize*i;
+        size_t offset   = (size_t)(slotsize-typesize);
+        memcpy(slot + offset, item, size);
+    }
     return i;
 }
+#endif/*  POOL_HEADER_ONLY   */
 
-#endif // POOL_HEADER_ONLY
 
-#endif//__EOBJECT_POOL_H
+#ifdef   HC_POOL_STRIP_PREFIX
+#define     pool_reserve        hc_pool_reserve
+#define     pool_release        hc_pool_release
+#define     pool_insert         hc_pool_insert
+#define     pool_from_heap      hc_pool_from_heap
+#define     pool_from_buffer    hc_pool_from_buffer
+#define     pool_grow_buffer    hc_pool_grow_buffer
+#define     pool_resize_buffer  hc_pool_resize_buffer
+
+#define     pool_slot_size        hc_pool_slot_size   
+#define     pool_type_size        hc_pool_type_size   
+#define     pool_end              hc_pool_end
+#define     pool_get              hc_pool_get
+#define     pool_measure          hc_pool_measure
+#define     pool_is_slot_used     hc_pool_is_slot_used
+
+#define     pool_measure_ex           hc_pool_measure_ex
+#define     pool_is_index_ok          hc_pool_is_index_ok
+#define     pool_assert_index         hc_pool_assert_index
+#define     pool_has_space            hc_pool_has_space
+#define     pool_need_resize          hc_pool_need_resize
+
+#define     pool_from_buffer_ex       hc_pool_from_buffer_ex
+#define     pool_from_heap_ex         hc_pool_from_heap_ex
+
+#define     pool_resize_buffer_ex     hc_pool_resize_buffer_ex
+#define     pool_grow_buffer_ex       hc_pool_grow_buffer_ex
+#define     pool_heap_free            hc_pool_heap_free
+
+#define     pool_reserve_ex           hc_pool_reserve_ex
+#define     pool_insert_ex            hc_pool_insert_ex
+#define     pool_release_ex           hc_pool_release_ex
+
+#define     pool_foreach              hc_pool_foreach
+
+#endif /* HC_POOL_STRIP_PREFIX*/
+
+#endif/* __HC_POOL_H    */
